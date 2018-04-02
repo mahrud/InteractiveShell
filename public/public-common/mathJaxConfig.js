c7ab2eaff314977c20628785578a546d38c399f0
pzinn <pzinnjustin@gmail.com>
2018-04-02 17:30:37 +1000

    fix #3

--- public/public-common/mathJaxConfig.js
+++ public/public-common/mathJaxConfig.js
@@ -1,21 +1,24 @@
 /* global MathJax */
 /* eslint "new-cap": "off" */
-MathJax.Hub.Config({tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}});
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
+});
 MathJax.Hub.Config({
   TeX: {noErrors: {disabled: true}}
 });
 MathJax.Hub.Config({
   TeX: {
     Macros: {
-      PP: '{\\mathbb{P}}',
-      ZZ: '{\\mathbb{Z}}',
-      QQ: '{\\mathbb{Q}}',
-      RR: '{\\mathbb{R}}',
-      CC: '{\\mathbb{C}}',
-      mac: '{{\\it Macaulay2}}',
-      bold: ['{\\bf #1}', 1],
+      PP: "{\\mathbb{P}}",
+      ZZ: "{\\mathbb{Z}}",
+      QQ: "{\\mathbb{Q}}",
+      RR: "{\\mathbb{R}}",
+      CC: "{\\mathbb{C}}",
+      mac: "{{\\it Macaulay2}}",
+      bold: ["{\\bf #1}", 1]
     },
-  },
+    MAXBUFFER: 20 * 1024
+  }
 });
 MathJax.Hub.Config({
   "CommonHTML": {linebreaks: {automatic: true}},
